87a360e1418938e740c3e386fa95174775bef09c
fabiangord <fabian_0227@hotmail.com>
2023-02-03 18:52:46 -0500

    se devuelve el true al secure

--- controllers/authController.js
+++ controllers/authController.js
@@ -34,7 +34,7 @@ const registerUser = async(req,res)=>{
             host: "smtp.mailtrap.io",
             port: 2525,
             auth: {
-                user: process.env.USER,
+                user: process.env.USERS,
                 pass: process.env.PASS
             }
           });
